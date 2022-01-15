540217f01fafb046643eb07982d5ce7418cccf86
SpiritCroc <dev@spiritcroc.de>
2022-01-15 10:25:14 +0100

    [DNM] Let's get some feel for slow sync issues
    Change-Id: I7db909870bd3fa0f4b2c242edd4c771c69d319a3

--- matrix-sdk-android/src/main/java/org/matrix/android/sdk/internal/session/sync/SyncResponseHandler.kt
+++ matrix-sdk-android/src/main/java/org/matrix/android/sdk/internal/session/sync/SyncResponseHandler.kt
@@ -75,95 +75,146 @@ internal class SyncResponseHandler @Inject constructor(
                                reporter: ProgressReporter?) {
         val isInitialSync = fromToken == null
         Timber.v("Start handling sync, is InitialSync: $isInitialSync")
+        android.util.Log.i("SCSCSC-SYNC", "Start handling sync, is InitialSync: $isInitialSync")
 
         measureTimeMillis {
+            android.util.Log.i("SCSCSC-SYNC", "check crypto-service started")
             if (!cryptoService.isStarted()) {
                 Timber.v("Should start cryptoService")
+                android.util.Log.i("SCSCSC-SYNC", "Should start cryptoService")
                 cryptoService.start()
+                android.util.Log.i("SCSCSC-SYNC", "started cryptoService")
             }
+            android.util.Log.i("SCSCSC-SYNC", "pre-onSyncWillProcess")
             cryptoService.onSyncWillProcess(isInitialSync)
+            android.util.Log.i("SCSCSC-SYNC", "post-onSyncWillProcess")
         }.also {
             Timber.v("Finish handling start cryptoService in $it ms")
+            android.util.Log.i("SCSCSC-SYNC", "Finish handling start cryptoService in $it ms")
         }
 
         // Handle the to device events before the room ones
         // to ensure to decrypt them properly
         measureTimeMillis {
             Timber.v("Handle toDevice")
+            android.util.Log.i("SCSCSC-SYNC", "Handle toDevice")
             reportSubtask(reporter, InitSyncStep.ImportingAccountCrypto, 100, 0.1f) {
+                android.util.Log.i("SCSCSC-SYNC", "Handle toDevice -01")
                 if (syncResponse.toDevice != null) {
+                    android.util.Log.i("SCSCSC-SYNC", "Handle toDevice -02")
                     cryptoSyncHandler.handleToDevice(syncResponse.toDevice, reporter)
+                    android.util.Log.i("SCSCSC-SYNC", "Handle toDevice -03")
                 }
+                android.util.Log.i("SCSCSC-SYNC", "Handle toDevice -04")
             }
         }.also {
             Timber.v("Finish handling toDevice in $it ms")
+            android.util.Log.i("SCSCSC-SYNC", "Finish handling toDevice in $it ms")
         }
+        android.util.Log.i("SCSCSC-SYNC", "pre-aggregator")
         val aggregator = SyncResponsePostTreatmentAggregator()
+        android.util.Log.i("SCSCSC-SYNC", "post-aggregator | pre fetchRoot")
 
         // Prerequisite for thread events handling in RoomSyncHandler
         threadsAwarenessHandler.fetchRootThreadEventsIfNeeded(syncResponse)
+        android.util.Log.i("SCSCSC-SYNC", "post-fetchRootHreaed | pre big transaction")
 
         // Start one big transaction
         monarchy.awaitTransaction { realm ->
             measureTimeMillis {
                 Timber.v("Handle rooms")
+                android.util.Log.i("SCSCSC-SYNC", "Handle rooms")
                 reportSubtask(reporter, InitSyncStep.ImportingAccountRoom, 1, 0.7f) {
+                    android.util.Log.i("SCSCSC-SYNC", "Handle rooms -01")
                     if (syncResponse.rooms != null) {
+                        android.util.Log.i("SCSCSC-SYNC", "Handle rooms -02")
                         roomSyncHandler.handle(realm, syncResponse.rooms, isInitialSync, aggregator, reporter)
+                        android.util.Log.i("SCSCSC-SYNC", "Handle rooms -03")
                     }
+                    android.util.Log.i("SCSCSC-SYNC", "Handle rooms -04")
                 }
             }.also {
                 Timber.v("Finish handling rooms in $it ms")
+                android.util.Log.i("SCSCSC-SYNC", "Finish handling rooms in $it ms")
             }
 
             measureTimeMillis {
+                android.util.Log.i("SCSCSC-SYNC", "pre-Handle groups")
                 reportSubtask(reporter, InitSyncStep.ImportingAccountGroups, 1, 0.1f) {
                     Timber.v("Handle groups")
+                    android.util.Log.i("SCSCSC-SYNC", "Handle groups")
                     if (syncResponse.groups != null) {
+                        android.util.Log.i("SCSCSC-SYNC", "Handle groups -01")
                         groupSyncHandler.handle(realm, syncResponse.groups, reporter)
+                        android.util.Log.i("SCSCSC-SYNC", "Handle groups -02")
                     }
+                    android.util.Log.i("SCSCSC-SYNC", "Handle groups -03")
                 }
             }.also {
                 Timber.v("Finish handling groups in $it ms")
+                android.util.Log.i("SCSCSC-SYNC", "Finish handling groups in $it ms")
             }
 
             measureTimeMillis {
+                android.util.Log.i("SCSCSC-SYNC", "pre-accountData")
                 reportSubtask(reporter, InitSyncStep.ImportingAccountData, 1, 0.1f) {
                     Timber.v("Handle accountData")
+                    android.util.Log.i("SCSCSC-SYNC", "Handle accountData -01")
                     userAccountDataSyncHandler.handle(realm, syncResponse.accountData)
+                    android.util.Log.i("SCSCSC-SYNC", "Handle accountData -02")
                 }
             }.also {
                 Timber.v("Finish handling accountData in $it ms")
+                android.util.Log.i("SCSCSC-SYNC", "Finish handling accountData in $it ms")
             }
 
             measureTimeMillis {
                 Timber.v("Handle Presence")
+                android.util.Log.i("SCSCSC-SYNC", "presence -01")
                 presenceSyncHandler.handle(realm, syncResponse.presence)
+                android.util.Log.i("SCSCSC-SYNC", "presence -02")
             }.also {
                 Timber.v("Finish handling Presence in $it ms")
+                android.util.Log.i("SCSCSC-SYNC", "Finish handling Presence in $it ms")
             }
+            android.util.Log.i("SCSCSC-SYNC", "pre sync-save-token")
             tokenStore.saveToken(realm, syncResponse.nextBatch)
+            android.util.Log.i("SCSCSC-SYNC", "post sync-save-token")
         }
 
         // Everything else we need to do outside the transaction
+        android.util.Log.i("SCSCSC-SYNC", "pre-aggregator-handle")
         aggregatorHandler.handle(aggregator)
+        android.util.Log.i("SCSCSC-SYNC", "post-aggregator-handle")
 
         syncResponse.rooms?.let {
+            android.util.Log.i("SCSCSC-SYNC", "check-push -01")
             checkPushRules(it, isInitialSync)
+            android.util.Log.i("SCSCSC-SYNC", "check-push -02 / sync invite with server")
             userAccountDataSyncHandler.synchronizeWithServerIfNeeded(it.invite)
+            android.util.Log.i("SCSCSC-SYNC", "check-push -03 / synced invite with server")
             dispatchInvitedRoom(it)
+            android.util.Log.i("SCSCSC-SYNC", "check-push -04 / dispatced invite")
         }
+        android.util.Log.i("SCSCSC-SYNC", "sync response rooms handled")
         syncResponse.groups?.let {
+            android.util.Log.i("SCSCSC-SYNC", "pre-schedule group data fetching")
             scheduleGroupDataFetchingIfNeeded(it)
+            android.util.Log.i("SCSCSC-SYNC", "post-schedule group data fetching")
         }
 
         Timber.v("On sync completed")
+        android.util.Log.i("SCSCSC-SYNC", "On sync completed")
         cryptoSyncHandler.onSyncCompleted(syncResponse)
+        android.util.Log.i("SCSCSC-SYNC", "On sync completed | cryptohandler knows")
 
         // post sync stuffs
         monarchy.writeAsync {
+            android.util.Log.i("SCSCSC-SYNC", "pre-write-monarchy")
             roomSyncHandler.postSyncSpaceHierarchyHandle(it)
+            android.util.Log.i("SCSCSC-SYNC", "post-write-monarchy")
         }
+        android.util.Log.i("SCSCSC-SYNC", "finally done")
     }
 
     private fun dispatchInvitedRoom(roomsSyncResponse: RoomsSyncResponse) {
